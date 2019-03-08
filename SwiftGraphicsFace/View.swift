85ff9182c4f2ec908d4cae68d20dea6dd40416be
Robert Muckle-Jones <robert.mucklejones@gmail.com>
2019-03-08 17:28:01 +0000

    initial commit

---
+++ SwiftGraphicsFace/View.swift
@@ -0,0 +1,16 @@
+//
+//  View.swift
+//  SwiftGraphicsFace
+//
+//  Created by Robert Muckle-Jones on 08/03/2019.
+//  Copyright © 2019 Robert Muckle-Jones. All rights reserved.
+//
+import Cocoa
+
+class View: NSView {
+    
+    override func draw(_ dirtyRect: NSRect) {
+        
+    }
+    
+}
---
+++ SwiftGraphicsFace/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftGraphicsFace
+//
+//  Created by Robert Muckle-Jones on 07/03/2019.
+//  Copyright © 2019 Robert Muckle-Jones. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
